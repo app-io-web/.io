71ecac0cfce61b9f4be7cb173c0b643ac8b56887
app-io-web <joaocarloslopesbarbosa@gmail.com>
2025-02-28 19:54:06 -0300

    Designer CADASTRO

--- src/App.js
+++ src/App.js
@@ -5,10 +5,7 @@ import Signup from './components/Signup'; // Certifique-se de que o caminho para
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Registro de Namorados</h1> {/* Altere para um título apropriado para o formulário de cadastro */}
         <Signup /> {/* Incluindo o componente Signup */}
-      </header>
     </div>
   );
 }
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
-import { v4 as uuidv4 } from 'uuid'; // Certifique-se de instalar a biblioteca uuid com `npm install uuid`
+import { v4 as uuidv4 } from 'uuid';
+import './Signup.css'; // Ajuste o caminho conforme necessário
 
 function Signup() {
   const [email, setEmail] = useState('');
@@ -8,51 +9,8 @@ function Signup() {
   const [nomeNamorado, setNomeNamorado] = useState('');
   const [nomeNamorada, setNomeNamorada] = useState('');
   const [dataInicioNamoro, setDataInicioNamoro] = useState('');
-  const [localizacaoNamorado, setLocalizacaoNamorado] = useState('');
-  const [localizacaoNamorada, setLocalizacaoNamorada] = useState('');
-  const [loadingLocation, setLoadingLocation] = useState(false);
-  const [locationError, setLocationError] = useState('');
-
-  useEffect(() => {
-    checkGeolocationSupport();
-  }, []);
-
-  const checkGeolocationSupport = () => {
-    if (navigator.permissions && navigator.geolocation) {
-      navigator.permissions.query({ name: "geolocation" }).then(function(result) {
-        if (result.state === "granted") {
-          getLocation();
-        } else if (result.state === "prompt") {
-          setLocationError("Clique no botão para permitir acesso à localização.");
-        } else {
-          setLocationError("Acesso à localização foi negado. Para habilitar, vá para Configurações > Privacidade > Serviços de Localização > [Seu App].");
-        }
-      });
-    } else {
-      setLocationError("Geolocalização não é suportada por este navegador.");
-    }
-  };
-
-  const getLocation = () => {
-    if (navigator.geolocation) {
-      setLoadingLocation(true);
-      navigator.geolocation.getCurrentPosition(success, handleError);
-    } else {
-      setLocationError("Geolocalização não é suportada por este navegador.");
-    }
-  };
-
-  const success = (position) => {
-    setLocalizacaoNamorado(`${position.coords.latitude}, ${position.coords.longitude}`);
-    setLocalizacaoNamorada(`${position.coords.latitude}, ${position.coords.longitude}`);
-    setLoadingLocation(false);
-  };
-
-  const handleError = (error) => {
-    console.warn(`ERRO(${error.code}): ${error.message}`);
-    setLoadingLocation(false);
-    setLocationError("Não foi possível acessar sua localização. Verifique as permissões do navegador.");
-  };
+  const [isCasado, setIsCasado] = useState(false);
+  const [dataInicioCasamento, setDataInicioCasamento] = useState('');
 
   const handleSignup = async () => {
     const idUnic = uuidv4();
@@ -73,10 +31,8 @@ function Signup() {
         NomeNamorada: nomeNamorada,
         Data_Inicio_Namoro: dataInicioNamoro,
         ID_UNIC: idUnic,
-        Localizacao_do_Namorado: localizacaoNamorado,
-        Localizacao_da_Namorada: localizacaoNamorada,
-        UnicNameNamorado: unicNameNamorado,
-        UnicNameNamorada: unicNameNamorada
+        Casado: isCasado,
+        Data_Inicio_Casamento: isCasado ? dataInicioCasamento : null
       }, config);
       console.log('Cadastro realizado:', response.data);
     } catch (error) {
@@ -85,16 +41,40 @@ function Signup() {
   };
 
   return (
-    <div>
-      {loadingLocation && <p>Carregando localização...</p>}
-      {locationError && <p>{locationError}</p>}
-      <button onClick={getLocation}>Obter Localização</button>
-      <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
-      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Senha" />
-      <input type="text" value={nomeNamorado} onChange={(e) => setNomeNamorado(e.target.value)} placeholder="Nome do Namorado" />
-      <input type="text" value={nomeNamorada} onChange={(e) => setNomeNamorada(e.target.value)} placeholder="Nome da Namorada" />
-      <input type="date" value={dataInicioNamoro} onChange={(e) => setDataInicioNamoro(e.target.value)} placeholder="Data de Início do Namoro" />
-      <button onClick={handleSignup}>Cadastrar</button>
+    <div className="container">
+      <h1 className='CadastroH1'>Cadastro</h1>
+      <div className="inputGroup">
+        <label className="label">Email: </label>
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="input" />
+      </div>
+      <div className="inputGroup">
+        <label className="label">Senha: </label>
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" />
+      </div>
+      <div className="inputGroup">
+        <label className="label">Seu Primeiro Nome(Namorado): </label>
+        <input type="text" value={nomeNamorado} onChange={(e) => setNomeNamorado(e.target.value)} className="input" />
+      </div>
+      <div className="inputGroup">
+        <label className="label">Primeiro Nome(Namorada): </label>
+        <input type="text" value={nomeNamorada} onChange={(e) => setNomeNamorada(e.target.value)} className="input" />
+      </div>
+      <div className="inputGroup">
+        <label className="label">Data de Início de Namoro: </label>
+        <input type="date" value={dataInicioNamoro} onChange={(e) => setDataInicioNamoro(e.target.value)} className="input" />
+      </div>
+      <div className="inputGroup">
+        <label className="label">Já Casaram?: </label>
+        <input type="checkbox" checked={isCasado} onChange={() => setIsCasado(!isCasado)} />
+        <label className="label">Sim</label>
+      </div>
+      {isCasado && (
+        <div className="inputGroup">
+          <label className="label">Data de Início de Casamento: </label>
+          <input type="date" value={dataInicioCasamento} onChange={(e) => setDataInicioCasamento(e.target.value)} className="input" />
+        </div>
+      )}
+      <button onClick={handleSignup} className="button">Cadastrar</button>
     </div>
   );
 }
